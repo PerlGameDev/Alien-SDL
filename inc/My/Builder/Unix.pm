a9df354aae4328696ba41a83453cfccef9bba21b
Kartik Thakore <thakore.kartik@gmail.com>
2010-02-23 09:31:03 -0500

    Cleaned up teh arch name regex for mac and 64 bit

--- inc/My/Builder/Unix.pm
+++ inc/My/Builder/Unix.pm
@@ -96,7 +96,7 @@ sub _get_configure_cmd {
 
   # NOTE: all ugly IFs concerning ./configure params have to go here
 
-  if(($pack eq 'SDL_gfx') && ($Config{archname} =~ /64/i || $Config{archname} =~ /2level/)) {
+  if(($pack eq 'SDL_gfx') && $Config{archname} =~ /64|2level/i) {
     $extra .= ' --disable-mmx';
   }
 
