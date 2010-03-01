e2088001764a73937f6b8f482c2059744fda56f5
kmx <kmx@cpan.org>
2010-03-01 22:45:32 +0100

    Better support for GL/gl.h + mingw-w64.sf.net gcc compiler

--- lib/Alien/SDL.pm
+++ lib/Alien/SDL.pm
@@ -218,6 +218,10 @@ sub check_header {
   $inc .= "#include <$_>\n" for @header;  
   syswrite($fs, <<MARKER); # write test source code
 #include <stdio.h>
+#ifdef _WIN32
+/* GL/gl.h on Win32 requires windows.h being included before */
+#include <windows.h>
+#endif
 $inc
 int demofunc(void) { return 0; }
 
