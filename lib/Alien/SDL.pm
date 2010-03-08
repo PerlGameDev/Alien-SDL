16db376fa25947fd9db84355763baadb7bbbd021
kthakore <kthakore@kasdlperl.ath.cx>
2010-03-08 14:32:09 +0000

    Updated Alien version to 0.9.1 for releasing

--- lib/Alien/SDL.pm
+++ lib/Alien/SDL.pm
@@ -15,11 +15,12 @@ Alien::SDL - building, finding and using SDL binaries
 
 =head1 VERSION
 
-Version 0.9.
+Version 0.9.1
 
 =cut
 
-our $VERSION = '0.9';
+our $VERSION = '0.9.1';
+$VERSION = eval $VERSION;
 
 =head1 SYNOPSIS
 
