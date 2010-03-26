7ad3eab2190500d82bee74082b2d0add3a6aafae
Dustin Mays <dork.fish.wat@gmail.com>
2010-03-26 03:47:13 -0400

    Fixed more 'additional_cflafs' misspellings.

--- lib/Alien/SDL.pm
+++ lib/Alien/SDL.pm
@@ -252,7 +252,7 @@ sub _sdl_config_via_script
   my $val = `$script --$param 2>$devnull`;
   $val =~ s/[\r\n]*$//;
   if($param eq 'cflags') {
-    $val .= ' ' . Alien::SDL::ConfigData->config('additional_cflafs');
+    $val .= ' ' . Alien::SDL::ConfigData->config('additional_cflags');
   }
   elsif($param eq 'libs') {
     $val .= ' ' . Alien::SDL::ConfigData->config('additional_libs');
@@ -272,7 +272,7 @@ sub _sdl_config_via_config_data
   return unless $val;
   # handle additional flags
   if($param eq 'cflags') {
-    $val .= ' ' . Alien::SDL::ConfigData->config('additional_cflafs');
+    $val .= ' ' . Alien::SDL::ConfigData->config('additional_cflags');
   }
   elsif($param eq 'libs') {
     $val .= ' ' . Alien::SDL::ConfigData->config('additional_libs');
