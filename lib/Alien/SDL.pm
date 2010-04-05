4ed3f95a2d2ea7dfe52d7d13e2e49a15234400bd
Kartik Thakore <thakore.kartik@gmail.com>
2010-04-05 17:01:56 +0000

    Updated version. We need to test this

--- lib/Alien/SDL.pm
+++ lib/Alien/SDL.pm
@@ -15,11 +15,11 @@ Alien::SDL - building, finding and using SDL binaries
 
 =head1 VERSION
 
-Version 1.1
+Version 1.2
 
 =cut
 
-our $VERSION = '1.1';
+our $VERSION = '1.2';
 $VERSION = eval $VERSION;
 
 =head1 SYNOPSIS
