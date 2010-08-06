f1942923db88f46bac0132912676e19766bbdee5
kthakore <thakore.kartik@gmail.com>
2010-08-06 14:53:23 +0000

    Updated to version 1.413

--- lib/Alien/SDL.pm
+++ lib/Alien/SDL.pm
@@ -14,11 +14,11 @@ Alien::SDL - building, finding and using SDL binaries
 
 =head1 VERSION
 
-Version 1.412
+Version 1.413
 
 =cut
 
-our $VERSION = '1.412';
+our $VERSION = '1.413';
 $VERSION = eval $VERSION;
 
 =head1 SYNOPSIS
