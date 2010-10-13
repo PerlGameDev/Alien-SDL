a670e7e16e96c1250259200ad94c4493e456201f
Tobias Leich <tobias.leich@adckrone.com>
2010-10-13 23:29:10 +0200

    version is now 1.417

--- lib/Alien/SDL.pm
+++ lib/Alien/SDL.pm
@@ -15,11 +15,11 @@ Alien::SDL - building, finding and using SDL binaries
 
 =head1 VERSION
 
-Version 1.416
+Version 1.417
 
 =cut
 
-our $VERSION = '1.416';
+our $VERSION = '1.417';
 $VERSION = eval $VERSION;
 
 =head1 SYNOPSIS
