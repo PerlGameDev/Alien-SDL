8e658652dcde678e029227f3502b262a931c79bb
Tobias Leich <email@froggs.de>
2011-12-31 20:03:39 +0100

    new dev release due to messed up MANIFEST file before

--- lib/Alien/SDL.pm
+++ lib/Alien/SDL.pm
@@ -16,11 +16,11 @@ Alien::SDL - building, finding and using SDL binaries
 
 =head1 VERSION
 
-Version 1.428_4
+Version 1.428_5
 
 =cut
 
-our $VERSION = '1.428_4';
+our $VERSION = '1.428_5';
 $VERSION = eval $VERSION;
 
 =head1 SYNOPSIS
