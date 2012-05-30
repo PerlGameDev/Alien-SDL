ab5a6f47a5076add44d2a2afcdee432c9304f003
Tobias Leich <email@froggs.de>
2012-05-30 23:16:25 +0200

    release 1.432 (fix for ogg-files and for ActivePerl on Windows)

--- lib/Alien/SDL.pm
+++ lib/Alien/SDL.pm
@@ -16,11 +16,11 @@ Alien::SDL - building, finding and using SDL binaries
 
 =head1 VERSION
 
-Version 1.431_3
+Version 1.432
 
 =cut
 
-our $VERSION = '1.431_3';
+our $VERSION = '1.432';
 $VERSION = eval $VERSION;
 
 =head1 SYNOPSIS
