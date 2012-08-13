3572215255661ff5f420d81d3be8fbbe4ed72134
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2012-08-12 20:15:06 -0400

    Release 1.439_1

--- lib/Alien/SDL.pm
+++ lib/Alien/SDL.pm
@@ -16,11 +16,11 @@ Alien::SDL - building, finding and using SDL binaries
 
 =head1 VERSION
 
-Version 1.438
+Version 1.439_1
 
 =cut
 
-our $VERSION = '1.438';
+our $VERSION = '1.439_1';
 $VERSION = eval $VERSION;
 
 =head1 SYNOPSIS
