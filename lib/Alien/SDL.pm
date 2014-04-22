9382ac2def7d28e34b406ba1a90138b6f5399736
Tobias Leich <email@froggs.de>
2014-04-22 23:35:44 +0200

    release 1.442

--- lib/Alien/SDL.pm
+++ lib/Alien/SDL.pm
@@ -16,11 +16,11 @@ Alien::SDL - building, finding and using SDL binaries
 
 =head1 VERSION
 
-Version 1.440
+Version 1.442
 
 =cut
 
-our $VERSION = '1.440';
+our $VERSION = '1.442';
 $VERSION = eval $VERSION;
 
 =head1 SYNOPSIS
